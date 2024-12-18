970c1fcedb66a78cab2524ade466f50325c633dc
Arohee Bhoja <abhoja@fbi-van.wifi.local.cmu.edu>
2024-12-18 00:09:49 -0500

    added commutator identities

--- Mathlib/Algebra/Group/Commutator.lean
+++ Mathlib/Algebra/Group/Commutator.lean
@@ -5,6 +5,8 @@ Authors: Thomas Browning
 -/
 import Mathlib.Algebra.Group.Defs
 import Mathlib.Data.Bracket
+import Mathlib.Algebra.Group.Basic
+-- import Mathlib.Algebra.Group.Conj
 
 /-!
 # The bracket on a group given by commutator.
@@ -20,3 +22,29 @@ instance commutatorElement {G : Type*} [Group G] : Bracket G G :=
 theorem commutatorElement_def {G : Type*} [Group G] (g₁ g₂ : G) :
     ⁅g₁, g₂⁆ = g₁ * g₂ * g₁⁻¹ * g₂⁻¹ :=
   rfl
+
+example {G : Type*} [Group G] (g₁ g₂ : G) : g₁ * g₂ * g₁⁻¹ = ⁅g₁, g₂⁆ * g₂ := by
+  rw [commutatorElement_def, inv_mul_cancel_right]
+
+example {G : Type*} [Group G] (g₁ g₂ : G) : ⁅g₂, g₁⁆ = ⁅g₁, g₂⁆⁻¹ := by
+  simp only [commutatorElement_def, mul_inv_rev, inv_inv, mul_assoc]
+
+example {G : Type*} [Group G] (g₁ g₂ g₃ : G) :
+  ⁅g₁, g₂ * g₃⁆ = ⁅g₁, g₂⁆ * (g₂ * ⁅g₁, g₃⁆ * g₂⁻¹) := by
+  simp only [commutatorElement_def, ← mul_assoc]
+  rw [inv_mul_cancel_right, inv_mul_cancel_right]
+  rw [mul_inv_rev, ← mul_assoc]
+
+example {G : Type*} [Group G] (g₁ g₂ g₃ : G) :
+  ⁅g₃ * g₁, g₂⁆ = g₃ * ⁅g₁, g₂⁆ * g₃⁻¹ * ⁅g₃, g₂⁆ := by
+  simp only [commutatorElement_def, ← mul_assoc]
+  rw [inv_mul_cancel_right, inv_mul_cancel_right]
+  rw [mul_inv_rev, ← mul_assoc]
+
+example {G : Type*} [Group G] (g₁ g₂ : G) : ⁅g₁, g₂⁻¹⁆ = g₂⁻¹ * ⁅g₂, g₁⁆ * g₂ := by
+  simp only [commutatorElement_def, mul_inv_rev, inv_inv, ← mul_assoc]
+  simp only [inv_mul_cancel, one_mul]
+
+example {G : Type*} [Group G] (g₁ g₂ : G) : ⁅g₁⁻¹, g₂⁆ = g₁⁻¹ * ⁅g₂, g₁⁆ * g₁ := by
+  simp only [commutatorElement_def, mul_inv_rev, inv_inv, ← mul_assoc]
+  rw [inv_mul_cancel_right]
